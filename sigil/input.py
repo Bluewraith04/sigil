3155a1f4cc8abaa2ce0161e40ce6f97b1ea4e181
Bluewraith04 <ogunwooluolasunmbo@gmail.com>
2025-02-15 19:46:36 +0100

    Add input dtype

--- sigil/input.py
+++ sigil/input.py
@@ -3,16 +3,17 @@ import numpy as np
 
 
 class Input(Node):
-    def __init__(self, expected_shape, name=None):
+    def __init__(self, expected_shape, name=None, dtype=np.float64):
         super().__init__(name)
         self.expected_shape = expected_shape
+        self.dtype = dtype
 
     def set_value(self, value):
-        self.value = np.array(value)
-        if self.value.shape[1:] != self.expected_shape:
-            raise ValueError(f"Expected input values with shape {("batch_size",) + self.expected_shape}")
+        self.value = np.array(value, dtype=self.dtype)
         if len(self.value.shape) == 1:
             self.value = self.value.reshape((1, -1))
+        if self.value.shape[1:] != self.expected_shape:
+            raise ValueError(f"Expected input values with shape {("batch_size",) + self.expected_shape}")
         
     def compute(self):
         pass
