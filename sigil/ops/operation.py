d94707442bcd904f44a64e58e54ffc8dbeb2d9de
Bluewraith04 <ogunwooluolasunmbo@gmail.com>
2025-02-17 13:50:10 +0100

    Add auto tracking for operations and update error message
    The updated Operation class automatically registers all operations with the active graph on construction. The TypeError message now provide more context.

--- sigil/ops/operation.py
+++ sigil/ops/operation.py
@@ -1,4 +1,5 @@
 from sigil.node import Node
+from sigil.graph import Graph
 
 
 class Operation(Node):
@@ -7,8 +8,16 @@ class Operation(Node):
         self.inputs = inputs
         for node in self.inputs:
             if not isinstance(node, Node):
-                raise TypeError("Add operation expected inputs to be a subclass of Node")
+                raise TypeError(
+                    f"{self.__class__.__name__} operation expected Node like input not {node.__class__.__name}")
             node.outputs.append(self)
+
+        active_graph = Graph.get_default_graph()
+        if active_graph:
+            for node in self.inputs:
+                if node not in active_graph.nodes:
+                    active_graph.add_node(node)
+            active_graph.add_node(self)
 
     def compute(self):
         raise NotImplementedError
