496ba9fd32bce62997786c35f8c85662d6edc80a
Bluewraith04 <ogunwooluolasunmbo@gmail.com>
2025-02-17 15:06:50 +0100

    Update package scope
    This change adds all new modules, packages, and classes to the project's __init__.py file allowing for direct imports.

--- sigil/__init__.py
+++ sigil/__init__.py
@@ -1,11 +1,32 @@
 from . import (
+    activations,
     node,
-    input,
+    io,
     variable,
     ops,
+    init,
+    graph,
+    loss,
+    optim
 )
 
+from .init import (
+    Initializer,
+    Zeros,
+    Ones,
+    RandomNormal,
+    RandomUniform,
+    GlorotNormal,
+    GlorotUniform,
+    HeNormal,
+    HeUniform,
+)
+
+from .graph import Graph
 from .ops import Operation, Add, MatMul
+from .activations import ReLU, Sigmoid, Softmax
 from .node import Node
-from .input import Input
+from .io import Input, DataLoader
 from .variable import Variable
+from .optim import Adam, RMSProp, SGD
+from .loss import BinaryCrossEntropyLoss, CrossEntropyLoss, MeanSquaredError
