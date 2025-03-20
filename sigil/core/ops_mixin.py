4b3f1dafc4615bdb768f18ed63a4a7810588ea84
Bluewraith04 <ogunwooluolasunmbo@gmail.com>
2025-03-20 22:25:51 +0100

    Create Mixin for Tensor Operations This update creates a mixin for all tensor ops keeping the Tensor class lightweight

---
+++ sigil/core/ops_mixin.py
@@ -0,0 +1,20 @@
+from base import OperationRegistry
+
+
+class ArithmeticOpsMixin:
+    """Mixin to provide arithmetic operations via the OperationRegistry."""
+
+    def __add__(self, other):
+        return OperationRegistry.get_op("add").forward(self, other)
+
+    def __sub__(self, other):
+        return OperationRegistry.get_op("sub").forward(self, other)
+
+    def __mul__(self, other):
+        return OperationRegistry.get_op("mul").forward(self, other)
+
+    def __truediv__(self, other):
+        return OperationRegistry.get_op("div").forward(self, other)
+
+    def __matmul__(self, other):
+        return OperationRegistry.get_op("matmul").forward(self, other)
