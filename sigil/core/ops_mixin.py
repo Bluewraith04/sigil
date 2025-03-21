5c70b0c8933780c3eaa71fa62010289ab4f1bbcf
Bluewraith04 <ogunwooluolasunmbo@gmail.com>
2025-03-21 10:48:38 +0100

    Update TensorOpsMixin This update adds support for more arithmetic operations including right-hand operations.

--- sigil/core/ops_mixin.py
+++ sigil/core/ops_mixin.py
@@ -18,3 +18,28 @@ class TensorOpsMixin:
 
     def __matmul__(self, other):
         return OperationRegistry.get_op("matmul").forward(self, other)
+
+    def __pow__(self, power, modulo=None):
+        return OperationRegistry.get_op("pow").forward(self, power)
+
+    def __radd__(self, other):
+        return OperationRegistry.get_op("add").forward(other, self)
+
+    def __rsub__(self, other):
+        return OperationRegistry.get_op("sub").forward(other, self)
+
+    def __rmul__(self, other):
+        return OperationRegistry.get_op("mul").forward(other, self)
+
+    def __rtruediv__(self, other):
+        return OperationRegistry.get_op("div").forward(other, self)
+
+    def __rpow__(self, other):
+        return OperationRegistry.get_op("pow").forward(other, self)
+
+    def __pos__(self):
+        return OperationRegistry.get_op("add").forward(0, self)
+
+    def __neg__(self):
+        return OperationRegistry.get_op("sub").forward(0, self)
+
